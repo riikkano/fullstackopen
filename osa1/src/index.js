a0cb4b4a097915a3565b2f07773c6d19b9606a9f
Riigs <riikkan0@gmail.com>
2018-07-12 17:58:05 +0300

    Lisäsin komponentin Hello ja propsin

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Luo komponentin App
-//renderöityy div-tagina
-const App = () => {
-const now = new Date()
-const a = 10
-const b = 20
-return (
-  <div>
-      <p>Hello world</p>
-      <p>React-sovelluksen komponentit ovat teknisesti ottaen Javascript funktioita</p>
-      <p>{a} plus {b} is {a + b}</p>
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello world {props.name}</p>
     </div>
   )
 }
 
-// renderöi komponentin sisällön tiedoston public/index.html määrittelemään div-elementtiin, jonka id:n arvona on ‘root’
+const App = () => {
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Arto" />
+      <Hello name="Pekka"/>
+    </div>
+  )
+}
+
 ReactDOM.render(<App />, document.getElementById('root'))
