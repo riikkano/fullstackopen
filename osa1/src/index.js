a5e34eeb48b1ccfdca20c52b57e02cd49fcd8481
Riigs <riikkan0@gmail.com>
2018-07-12 17:21:55 +0300

    Lisäsin kommentteja muistiinpanoiksi

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Luo komponentin App
 const App = () => (
   <div>
     <p>Hello world</p>
   </div>
 )
 
+// renderöi komponentin sisällön tiedoston public/index.html määrittelemään div-elementtiin, jonka id:n arvona on ‘root’
 ReactDOM.render(<App />, document.getElementById('root'))
