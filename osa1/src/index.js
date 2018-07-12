779c823398f350c3e28b4797018b2bbff5cb35e9
Riigs <riikkan0@gmail.com>
2018-07-12 17:35:11 +0300

    Päivitys

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -2,11 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // Luo komponentin App
-const App = () => (
+//renderöityy div-tagina
+const App = () => {
+const now = new Date()
+const a = 10
+const b = 20
+return (
   <div>
-    <p>Hello world</p>
-  </div>
-)
+      <p>Hello world</p>
+      <p>React-sovelluksen komponentit ovat teknisesti ottaen Javascript funktioita</p>
+      <p>{a} plus {b} is {a + b}</p>
+    </div>
+  )
+}
 
 // renderöi komponentin sisällön tiedoston public/index.html määrittelemään div-elementtiin, jonka id:n arvona on ‘root’
 ReactDOM.render(<App />, document.getElementById('root'))
