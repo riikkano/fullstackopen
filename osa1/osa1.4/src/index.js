06dde88212a0d04309c561bc3d5e46ec91033d5c
Riigs <riikkan0@gmail.com>
2018-07-14 14:46:06 +0300

    Muokkasin osat taulukoksi

--- osa1/osa1.4/src/index.js
+++ osa1/osa1.4/src/index.js
@@ -21,9 +21,9 @@ const Sisalto = (props) => {
     return (
     <div>
     {/*props*/}
-      <Osa osa={props.osa1} tehtavia={props.tehtavia1}/>
-      <Osa osa={props.osa2} tehtavia={props.tehtavia2}/>
-      <Osa osa={props.osa3} tehtavia={props.tehtavia3}/>
+      <Osa osa={props.osat[0].nimi} tehtavia={props.osat[0].tehtavia}/>
+      <Osa osa={props.osat[1].nimi} tehtavia={props.osat[1].tehtavia}/>
+      <Osa osa={props.osat[2].nimi} tehtavia={props.osat[2].tehtavia}/>
     </div>
   )
 }
@@ -31,33 +31,34 @@ const Sisalto = (props) => {
 const Yhteensa = (props) => {
   return(
     <div>
-    <p>yhteensä {props.tehtavia1 + props.tehtavia2 + props.tehtavia3} tehtävää</p>
+    <p>yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää</p>
     </div>
   )
 }
 
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
-  const osa1 = {
-    nimi: 'Reactin perusteet',
-    tehtavia: 10
-  }
+  const osat = [
+    {
+      nimi: 'Reactin perusteet',
+      tehtavia: 10
+    },
+    {
+      nimi: 'Tiedonvälitys propseilla',
+      tehtavia: 7
+    },
+    {
+      nimi: 'Komponenttien tila',
+      tehtavia: 14
+    }
+  ]
 
-  const osa2 = {
-    nimi: 'Tiedonvälitys propseilla',
-    tehtavia: 7
-  }
-  const osa3 = {
-    nimi: 'Komponenttien tila',
-    tehtavia: 14
-  }
 
   return (
     <div>
       <Otsikko kurssi={kurssi}/>
-      <Sisalto />
-      <Sisalto osa1={osa1.nimi} tehtava1={osa1.tehtavia} osa2={osa2.nimi} tehtava2={osa2.tehtavia} osa3={osa3.nimi} tehtava3={osa3.tehtavia} />
-      <Yhteensa tehtavia1={osa1.tehtavia} tehtavia2={osa2.tehtavia} tehtavia3={osa3.tehtavia} />
+      <Sisalto osat={osat}/>
+      <Yhteensa osat={osat} />
     </div>
   )
 }
