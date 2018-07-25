3626fcbeca600250d47f2603144ad31573145093
Riigs <riikkan0@gmail.com>
2018-07-25 18:47:10 +0300

    Tehtävä 1.12
    Laajensin anekdootti -sovellusta niin, että se arpoo käyttäjälle toisen anekdootin nappia painamalla.

---
+++ osa1/anekdootit/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => {
+  return (
+    <button onClick={props.seuraavaAnekdootti}>
+	   next anecdote
+    </button>
+  )
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 0
+    }
+  }
+
+  arvoAnekdootti(len) {
+    console.log("Listan pituus", len)
+    return () => {
+    this.setState({ selected : [Math.floor((Math.random() * (len-1)) )] })
+    }
+  }
+
+  render() {
+    const len = this.props.anecdotes.length
+    return (
+      <div>
+        {this.props.anecdotes[this.state.selected]}
+        <br/>
+        <Button seuraavaAnekdootti={this.arvoAnekdootti(len)}/>
+        {console.log("Arvottu anekdootti:", this.state.selected)}
+      </div>
+    )
+  }
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
