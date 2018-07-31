3a2329a99b6ceab1b8014c80a2018812290b4f72
Riigs <riikkan0@gmail.com>
2018-07-31 17:05:58 +0300

    Tehtävä 2.5
    Erillinen moduuli komponentille Kurssi ja sen alikomponenteille.

--- osa2/osa2.1/src/index.js
+++ osa2/osa2.1/src/index.js
@@ -1,74 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
+import Kurssi from './components/Kurssi.js'
 
-const Kurssi = (props) => {
-  console.log(props.kurssi.nimi)
-    return(
-      <div>
-        <Otsikko kurssi={props.kurssi.nimi} />
-        <Sisalto kurssi={props.kurssi} />
-        <Yhteensa kurssi={props.kurssi} />
-      </div>
-    )
-}
+
 
 const Kurssit = (props) => {
   return (
-  <div>
-  {/*props*/}
-  {console.log("kurssit:", props.kurssit)}
-  <h1>Opetusohjelma</h1>
-    <ul>
-      {props.kurssit.map(kurssi =>
-          <li key={kurssi.id}>
-            <Kurssi kurssi={kurssi}/>
-          </li>)}
-    </ul>
-  </div>
-)
-}
-
-const Otsikko = (props) => {
-    return (
     <div>
-    <h2>{props.kurssi}</h2>
-    </div>
-  )
-}
-
-const Osa = (props) => {
-  return(
-    <div>
-    <p>{props.osa} {props.tehtavia}</p>
-    </div>
-  )
-}
-
-const Sisalto = (props) => {
-    return (
-    <div>
-    <ul>
-      {props.kurssi.osat.map(osa =>
-          <li key={osa.id}>
-            <Osa osa={osa.nimi} tehtavia={osa.tehtavia} />
-          </li>)}
-    </ul>
-    </div>
-
-  )
-}
-
-const Yhteensa = (props) => {
-  let initialValue = 0;
-  let sum = props.kurssi.osat.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue.tehtavia;
-    },initialValue)
-  console.log("Kurssin osat:", props.kurssi.osat)
-  console.log("Tehtävien summa:", sum) // logs 6
-  return(
-    <div>
-    <p>Yhteensä {sum} tehtävää</p>
+    {/*props*/}
+    {console.log("kurssit:", props.kurssit)}
+    <h1>Opetusohjelma</h1>
+      <ul>
+        {props.kurssit.map(kurssi =>
+            <li key={kurssi.id}>
+              <Kurssi kurssi={kurssi}/>
+            </li>)}
+      </ul>
     </div>
   )
 }
